daecca7cf29c341848aae3358f90d41e1654875d
dishenkothari <dishen.hsk@gmail.com>
2025-09-07 16:55:27 +0530

    Create smalest no.c

---
+++ smalest no.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+ printf("enter no.1 %d\n",a);
+scanf("%d",&a);
+printf("enter no.1 %d\n",b);
+scanf("%d",&b);
+
+if(a<b){
+    printf("a is smaller no.");
+
+}
+else{
+    printf("b is smaller no.");
+}
+    
+  
+
+
+    return 0;
+}
