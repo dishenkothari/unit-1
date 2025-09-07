aebae0184249e2fd5ac908476bb495cad3e92576
dishenkothari <dishen.hsk@gmail.com>
2025-09-07 16:54:56 +0530

    Create largest no.c

---
+++ largest no.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+ printf("enter no.1 %d\n",a);
+scanf("%d",&a);
+printf("enter no.1 %d\n",b);
+scanf("%d",&b);
+
+if(a>b){
+    printf("a is bigger no.");
+}
+else{
+    printf("b is bigger no.");
+}
+    return 0;
+}
