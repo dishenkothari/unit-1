1b26b444ee720a9212a4e76026d9cc23a1888f92
dishenkothari <dishen.hsk@gmail.com>
2025-09-07 17:40:43 +0530

    Create no.divisible by 7.c

---
+++ no.divisible by 7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   int a;
+   scanf("%d",&a);
+   if(a%7 == 0){
+       printf("no. is divisible by 7",a);
+   }
+   else{
+       printf("not divisible");
+   }
+}
