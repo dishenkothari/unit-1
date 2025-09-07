72cd3be3f3327221a209f01a8bd3af87a43ef1ed
dishenkothari <dishen.hsk@gmail.com>
2025-09-07 17:40:19 +0530

    Create net.c

---
+++ net.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    float gross, allowance, deduction, net;
+    scanf("%f", &gross);
+    if (gross > 10000) {
+        allowance = gross * 0.10;
+        deduction = gross * 0.03;
+    } else if (gross > 5000) {
+        allowance = gross * 0.07;
+        deduction = gross * 0.02;
+    } else {
+        allowance = 0;
+        deduction = 0;
+    }
+    net = gross + allowance - deduction;
+    printf("%f", net);
+    return 0;
+}
